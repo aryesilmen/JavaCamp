3e16fe7d64f1196fd256a838c428b556b78910a2
aryesilmen <100726302+aryesilmen@users.noreply.github.com>
2022-09-21 21:34:47 +0300

    Update and rename multiDimensionalArrayDemo.java to MultiDimensionalArrayDemo.java

--- Week1HW/MultiDimensionalArrayDemo.java
+++ Week1HW/MultiDimensionalArrayDemo.java
@@ -1,6 +1,6 @@
 package Week1HW;
 
-public class multiDimensionalArrayDemo {
+public class MultiDimensionalArrayDemo {
     public static void main(String[] args) {
         String[][] sehirler = new String[3][3];
         sehirler[0][0]= "İstanbul";
